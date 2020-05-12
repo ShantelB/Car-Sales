c1905414d24421ced676e95a3c20f7ee7e91134d
Shantel Brown <Shantel.Brown2021@gmail.com>
2020-05-12 16:04:47 -0700

    MVP met

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from "react-redux";
+import {removeItem} from '../actions/Action';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => props.removeItem(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  return {
+    additionalFeature: state.additionalFeature,
+    car: state.car,
+    additionalPrice: state.additionalPrice
+  };
+};
+
+export default connect(mapStateToProps, {removeItem}) (AddedFeature);
